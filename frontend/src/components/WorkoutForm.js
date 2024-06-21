0c401e3fea135a95506b434e4dbef5e6f73a3720
Muslim-Rana <muslim.inaam.rana@gmail.com>
2024-06-20 21:42:35 -0400

    Implemented error handling for incorrect input on the WorkoutForm component

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -7,8 +7,12 @@ const WorkoutForm = () => {
     const [load, setLoad] = useState('')
     const [reps, setReps] = useState('')
     const [error, setError] = useState(null)
+    const [emptyFields, setEmptyFields] = useState([])
 
     const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        
         const workout = {title, load, reps}
 
         const response = await fetch('/api/workouts', {
@@ -23,12 +27,14 @@ const WorkoutForm = () => {
 
         if (!response.ok) {
             setError(json.error)
+            setEmptyFields(json.emptyFields)
         }
         if (response.ok) {
             setTitle('')
             setLoad('')
             setReps('')
             setError(null)
+            setEmptyFields([])
             console.log('new workout added')
             dispatch({type: 'CREATE_WORKOUT', payload: json})
         }
@@ -43,6 +49,7 @@ const WorkoutForm = () => {
                 type = "text"
                 onChange={(e) => setTitle(e.target.value)}
                 value = {title}
+                className={emptyFields.includes('title') ? 'error' : ''}
             />
 
             <label>Load (in kg):</label>
@@ -50,6 +57,7 @@ const WorkoutForm = () => {
                 type = "number"
                 onChange={(e) => setLoad(e.target.value)}
                 value = {load}
+                className={emptyFields.includes('load') ? 'error' : ''}
             />
 
             <label>Reps:</label>
@@ -57,6 +65,7 @@ const WorkoutForm = () => {
                 type = "number"
                 onChange={(e) => setReps(e.target.value)}
                 value = {reps}
+                className={emptyFields.includes('reps') ? 'error' : ''}
             />
 
             <button>Add Workout</button>
